e2e233979d5891693e6eb7550e522d1042c0765d
Rob Gifford <robjgiff@gmail.com>
2019-08-30 18:36:31 +0100

    Cleaned up, removing superfluous files, project builds with complete dependo curated set inc. aligns

--- glue/loadExogenousData.js
+++ glue/loadExogenousData.js
@@ -11,3 +11,18 @@ _.each(seqIds, function(seqId) {
         glue.command(["set", "link-target", "isolate_data", "custom-table-row/isolate_data/"+seqId]);
     });
 });
+
+
+// list the sequences in source dependo-ncbi-curated
+var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'dependo-ncbi-curated'"]);
+// extract from the result a list of sequence IDs.
+var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
+// for each sequence ID
+_.each(seqIds, function(seqId) {
+    // create an object in the custom table which uses the sequence ID as the row ID.
+    glue.command(["create", "custom-table-row", "isolate_data", seqId]);
+    // associate the corresponding sequence with this object.
+    glue.inMode("sequence/dependo-ncbi-curated/"+seqId, function() {
+        glue.command(["set", "link-target", "isolate_data", "custom-table-row/isolate_data/"+seqId]);
+    });
+});
