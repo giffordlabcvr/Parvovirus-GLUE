a16b63111af387c4de61a35fc1006be1b3de7221
Rob Gifford <robjgiff@gmail.com>
2019-12-31 13:13:04 +0000

    Abstracted out extensions for curated sources and EVEs

--- glue/loadCuratedDependo.js
+++ glue/loadCuratedDependo.js
@@ -1,5 +1,5 @@
-// list the sequences in source ncbi-refseqs
-var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'ncbi-refseqs'"]);
+// list the sequences in source dependo-ncbi-curated
+var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'dependo-ncbi-curated'"]);
 // extract from the result a list of sequence IDs.
 var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
 // for each sequence ID
@@ -7,9 +7,8 @@ _.each(seqIds, function(seqId) {
     // create an object in the custom table which uses the sequence ID as the row ID.
     glue.command(["create", "custom-table-row", "isolate_data", seqId]);
     // associate the corresponding sequence with this object.
-    glue.inMode("sequence/ncbi-refseqs/"+seqId, function() {
+    glue.inMode("sequence/dependo-ncbi-curated/"+seqId, function() {
         glue.command(["set", "link-target", "isolate_data", "custom-table-row/isolate_data/"+seqId]);
     });
 });
-
 
