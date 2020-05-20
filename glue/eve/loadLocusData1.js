31a0523c68099f6cfc0706b28985a6199aabccb8
Rob Gifford <robjgiff@gmail.com>
2020-05-21 00:51:16 +0100

    Refactoring

--- glue/eve/loadLocusData1.js
+++ glue/eve/loadLocusData1.js
@@ -1,5 +1,6 @@
 // list the PVe sequences
-var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'fasta-curated-epv'"]);
+var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'fasta-digs-epv'"]);
+
 // extract from the result a list of sequence IDs.
 var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
 
@@ -8,7 +9,7 @@ _.each(seqIds, function(seqId) {
     // create an object in the custom table which uses the sequence ID as the row ID.
     glue.command(["create", "custom-table-row", "locus_data", seqId]);
     // associate the corresponding sequence with this object.
-    glue.inMode("sequence/fasta-curated-epv/"+seqId, function() {
+    glue.inMode("sequence/fasta-digs-epv/"+seqId, function() {
         glue.command(["set", "link-target", "locus_data", "custom-table-row/locus_data/"+seqId]);
     });
 });
