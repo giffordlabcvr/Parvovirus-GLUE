617fae3e621f8a46f79c504e5be9d000251aa2d4
Rob Gifford <robjgiff@gmail.com>
2020-06-26 19:08:51 +0100

    Refactoring

--- glue/eve/loadRefConData2.js
+++ glue/eve/loadRefConData2.js
@@ -7,17 +7,6 @@ glue.inMode("module/parvoviridaeTabularUtility", function() {
 
 _.each(loadResult, function(eveObj) {
 
-	glue.inMode("custom-table-row/refcon_data/"+eveObj.id, function() {
-	
-		// glue.log("INFO", "Processing sequence:", eveObj.id);
-
-
-		glue.command(["set", "field", "locus_numeric_id", eveObj.locus_numeric_id]);
-		glue.command(["set", "field", "reftype", eveObj.seq_type]);
-		glue.command(["set", "field", "host_group_taxlevel", eveObj.host_group_taxlevel]);
-		glue.command(["set", "field", "host_group_name", eveObj.host_group_name]);
-
-	});
 	
 	glue.inMode("sequence/fasta-refseqs-epv/"+eveObj.sequenceID, function() {
 	
