729e18d587595b1a018906e23d3aae122e0ba47d
Rob Gifford <robjgiff@gmail.com>
2020-07-03 00:17:00 +0100

    Updated side-data

--- glue/eve/loadRefConData2.js
+++ glue/eve/loadRefConData2.js
@@ -17,14 +17,14 @@ _.each(loadResult, function(eveObj) {
 
 	});
 
-	glue.inMode("sequence/"+source_name+"/"+eveObj.sequenceID, function() {
+	glue.inMode("sequence/fasta-refseqs-epv/"+eveObj.sequenceID, function() {
 	
 		glue.log("INFO", "Entering sequence table data for EVE reference:", eveObj.sequenceID);
 
-		glue.command(["set", "field", "clade_ns", eveObj.virus_clade_ns]);
-		glue.command(["set", "field", "subclade_ns", eveObj.virus_subclade_ns]);
-		glue.command(["set", "field", "clade_vp", eveObj.virus_clade_vp]);
-		glue.command(["set", "field", "subclade_vp", eveObj.virus_subclade_vp]);
+		//glue.command(["set", "field", "clade_ns", eveObj.virus_clade_ns]);
+		//glue.command(["set", "field", "subclade_ns", eveObj.virus_subclade_ns]);
+		//glue.command(["set", "field", "clade_vp", eveObj.virus_clade_vp]);
+		//glue.command(["set", "field", "subclade_vp", eveObj.virus_subclade_vp]);
 		glue.command(["set", "field", "name", eveObj.insertion_name]);
 		glue.command(["set", "field", "full_name", eveObj.insertion_full_name]);
 
