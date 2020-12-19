f97faeec3ab94fff35a5e3c7fc11362569b1a654
Rob Gifford <robjgiff@gmail.com>
2020-12-19 13:07:44 +0000

    Validated erythyro component

--- glue/genus/amdo/amdoLoadIsolateData1.js
+++ glue/genus/amdo/amdoLoadIsolateData1.js
@@ -1,25 +1,14 @@
-var masterRef = 'NC_001662';
-
-// list the sequences in source ncbi-refseqs-amdo-proto
-var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'ncbi-refseqs-amdo' and genus = 'Amdoparvovirus'"]);
-
+// list the sequences in source ncbi-refseqs-circo
+var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'ncbi-refseqs-amdo'"]);
 // extract from the result a list of sequence IDs.
 var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
-
 // for each sequence ID
 _.each(seqIds, function(seqId) {
-
-	if (seqId != masterRef) { // Skip master reference
-	
-	  // create an object in the custom table which uses the sequence ID as the row ID.
-	  glue.command(["create", "custom-table-row", "isolate_data", seqId]);
-	  
-	  // associate the corresponding sequence with this object.
-	  glue.inMode("sequence/ncbi-refseqs-amdo/"+seqId, function() {
-	  
-		  glue.command(["set", "link-target", "isolate_data", "custom-table-row/isolate_data/"+seqId]);
-		  
-	  });
-    }
+    // create an object in the custom table which uses the sequence ID as the row ID.
+    glue.command(["create", "custom-table-row", "isolate_data", seqId]);
+    // associate the corresponding sequence with this object.
+    glue.inMode("sequence/ncbi-refseqs-amdo/"+seqId, function() {
+        glue.command(["set", "link-target", "isolate_data", "custom-table-row/isolate_data/"+seqId]);
+    });
 });
 
--- glue/genus/erythyro/erythyroLoadIsolateData1.js
+++ glue/genus/erythyro/erythyroLoadIsolateData1.js
@@ -1,24 +1,14 @@
-var masterRef = 'NC_000883';
-
-// list the sequences in source ncbi-refseqs
-var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'ncbi-refseqs-erythyro' and genus = 'Erythyroparvovirus'"]);
-
+// list the sequences in source ncbi-refseqs-circo
+var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'ncbi-refseqs-erythyro'"]);
 // extract from the result a list of sequence IDs.
 var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
-
 // for each sequence ID
 _.each(seqIds, function(seqId) {
-
-	if (seqId != masterRef) { // Skip master reference
-	
-	  // create an object in the custom table which uses the sequence ID as the row ID.
-	  glue.command(["create", "custom-table-row", "isolate_data", seqId]);
-	  
-	  // associate the corresponding sequence with this object.
-	  glue.inMode("sequence/ncbi-refseqs-erythyro/"+seqId, function() {
-	  
-		  glue.command(["set", "link-target", "isolate_data", "custom-table-row/isolate_data/"+seqId]);
-	  });
-    }
+    // create an object in the custom table which uses the sequence ID as the row ID.
+    glue.command(["create", "custom-table-row", "isolate_data", seqId]);
+    // associate the corresponding sequence with this object.
+    glue.inMode("sequence/ncbi-refseqs-erythyro/"+seqId, function() {
+        glue.command(["set", "link-target", "isolate_data", "custom-table-row/isolate_data/"+seqId]);
+    });
 });
 
