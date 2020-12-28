32463d80682971227a35465fc754cd06f4e1bf8d
Robert Gifford <robjgiff@gmail.com>
2020-12-28 12:57:42 +0000

    Refactor

--- glue/eve/amdo/loadRefConData2.js
+++ glue/eve/amdo/loadRefConData2.js
@@ -27,13 +27,13 @@ _.each(loadResult, function(eveObj) {
 
 		glue.command(["set", "field", "subfamily", eveObj.virus_subfamily]);
 		glue.command(["set", "field", "genus", eveObj.virus_genus]);
-		glue.command(["set", "field", "assign_clade", eveObj.assign_clade]);
-		glue.command(["set", "field", "assign_subclade", eveObj.assign_subclade]);
+		//glue.command(["set", "field", "assign_clade", eveObj.assign_clade]);
+		//glue.command(["set", "field", "assign_subclade", eveObj.assign_subclade]);
 
-		glue.command(["set", "field", "clade_ns", eveObj.virus_clade_ns]);
-		glue.command(["set", "field", "subclade_ns", eveObj.virus_subclade_ns]);
-		glue.command(["set", "field", "clade_vp", eveObj.virus_clade_vp]);
-		glue.command(["set", "field", "subclade_vp", eveObj.virus_subclade_vp]);
+		//glue.command(["set", "field", "clade_ns", eveObj.virus_clade_ns]);
+		//glue.command(["set", "field", "subclade_ns", eveObj.virus_subclade_ns]);
+		//glue.command(["set", "field", "clade_vp", eveObj.virus_clade_vp]);
+		//glue.command(["set", "field", "subclade_vp", eveObj.virus_subclade_vp]);
 
 		//glue.command(["set", "field", "name", eveObj.insertion_name]);
 		//glue.command(["set", "field", "full_name", eveObj.insertion_full_name]);
