1aa7dd797f746e23bbe90dc1aba310021d1bfed8
Rob Gifford <robjgiff@gmail.com>
2021-02-16 11:22:35 +0000

    Refactoring

---
+++ glue/genus/icthama/icthamaLoadIsolateData1.js
@@ -0,0 +1,22 @@
+var masterRef = 'MN049932';
+
+// list the sequences in source ncbi-refseqs-icthama
+var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'ncbi-refseqs-icthama' and genus = 'Icthamaparvovirus'"]);
+
+// extract from the result a list of sequence IDs.
+var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
+
+// for each sequence ID
+_.each(seqIds, function(seqId) {
+
+	if (seqId != masterRef) { // Skip master reference
+	  // create an object in the custom table which uses the sequence ID as the row ID.
+	  glue.command(["create", "custom-table-row", "isolate_data", seqId]);
+	  // associate the corresponding sequence with this object.
+	  glue.inMode("sequence/ncbi-refseqs-icthama/"+seqId, function() {
+		  glue.command(["set", "link-target", "isolate_data", "custom-table-row/isolate_data/"+seqId]);
+	  });
+    }
+});
+
+
