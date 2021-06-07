65973bce14210a9ba445afcc94f7ccb4ed03464e
Rob Gifford <robjgiff@gmail.com>
2021-06-07 19:09:11 +0100

    Adding tetraparvovirus component plus other items

---
+++ glue/genus/tetra/deleteTetraIsolateTableRows.js
@@ -0,0 +1,12 @@
+
+// Delete custom-table table rows for tetra genus extension sequences
+ var resultList = glue.tableToObjects(glue.command(["list", "sequence", "sequenceID", "source.name", "-w", "source.name = 'ncbi-refseqs-tetra'"]));	
+ 	 
+ _.each(resultList,function(resultObj) {
+
+	var sequenceID = resultObj["sequenceID"];
+	glue.log("INFO", "Deleting isolate data table for:", sequenceID);
+	glue.command(["delete", "custom-table-row", "isolate_data", sequenceID]);				
+
+});
+
