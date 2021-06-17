0a519cfe00679ffaef6bd5c1ca0040945240e6fa
Rob Gifford <robjgiff@gmail.com>
2021-06-17 19:56:17 +0100

    Fixed misspelling of Erythyro

---
+++ glue/genus/erythro/deleteErythroIsolateTableRows.js
@@ -0,0 +1,12 @@
+
+// Delete custom-table table rows for erythro genus extension sequences
+ var resultList = glue.tableToObjects(glue.command(["list", "sequence", "sequenceID", "source.name", "-w", "source.name = 'ncbi-refseqs-erythro'"]));	
+ 	 
+ _.each(resultList,function(resultObj) {
+
+	var sequenceID = resultObj["sequenceID"];
+	glue.log("INFO", "Deleting isolate data table for:", sequenceID);
+	glue.command(["delete", "custom-table-row", "isolate_data", sequenceID]);				
+
+});
+
