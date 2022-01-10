f9de903f011184fcac9c5eba768f6589eed97ee2
Rob Gifford <robjgiff@gmail.com>
2022-01-10 15:57:04 +0000

    Refactor

---
+++ glue/analysis/msa/calculateRefseqAminoAcidComposition.js
@@ -0,0 +1,18 @@
+// Script to calculate nucleotide composition in reference sequences
+aaCompositionResults = {}
+
+// get list of reference sequences from GLUE
+
+// iterate through reference list
+
+  // export all coding features in this reference 
+  
+  // iterate through coding features
+    
+    // get amino acid composition of feature
+    
+    // store result
+    
+    
+// Export results
+
