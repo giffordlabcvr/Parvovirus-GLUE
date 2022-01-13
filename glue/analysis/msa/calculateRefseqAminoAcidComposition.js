d8f0249832c7dd65262a271b8a4da34af824a8ce
Rob Gifford <robjgiff@gmail.com>
2022-01-13 14:32:56 +0000

    Updated script

--- glue/analysis/msa/calculateRefseqAminoAcidComposition.js
+++ glue/analysis/msa/calculateRefseqAminoAcidComposition.js
@@ -2,19 +2,37 @@
 aaCompositionResults = {}
 
 // get list of reference sequences from GLUE
-// command list reference -w "" 
+
+var referencesList = glue.command(["list","reference"]);
+
+glue.log("INFO", "RESULT WAS ", referencesList);
 
 
-// iterate through reference list
 
-  // export all coding features in this reference 
+
+  // iterate through reference list
+
+	// export all coding features in this reference 
   
-  // iterate through coding features
-    
-    // get amino acid composition of feature
-    
-    // store result
-    
+	// iterate through coding features
+	
+	  // get amino acid sequence
+	
+	  // split the amino acid sequence into individual chars
+	
+		// iterate through get amino acid composition of feature
+
+    // store feature
+
+  // store result
+  
+
+
+// Transform the data 
+   
+   // Work out what kind of transformation we need to do 
+   
+   
     
 // Export results
 
