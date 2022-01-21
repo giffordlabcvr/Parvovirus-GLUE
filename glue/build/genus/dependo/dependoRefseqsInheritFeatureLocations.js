b93a78d96d049b05f590f2dbe06563e20acf11a0
Rob Gifford <robjgiff@gmail.com>
2022-01-21 17:18:20 +0000

    Refactoring - modules directory

--- glue/build/genus/dependo/dependoRefseqsInheritFeatureLocations.js
+++ glue/build/genus/dependo/dependoRefseqsInheritFeatureLocations.js
@@ -1,10 +1,17 @@
 // Remove feature location annotations all reference sequences except master
-glue.command(["multi-delete", "feature_location", "-w", "referenceSequence.name != 'REF_MASTER_Dependo_AAV2'"]);
+//glue.command(["multi-delete", "feature_location", "-w", "referenceSequence.name != 'REF_MASTER_Dependo_AAV2'"]);
+
+var constrainingRef = 'REF_MASTER_Dependo_AAV2';
 
 // Get list of features on master reference
 var featuresToInherit = get_features('REF_MASTER_Dependo_AAV2');
 
-//list all reference sequences
+
+//list all alignments constrained by 
+
+
+
+//list all dependo reference sequences
 var refSeqObjList = glue.tableToObjects(glue.command(["list", "reference", "name"]));
 
 
@@ -17,7 +24,7 @@ _.each(refSeqObjList, function(refSeqObj) {
 
 		 glue.inMode("reference/"+refSeqObj.name, function() {
 			 glue.command(["inherit", "feature-location", 			
-				 "AL_AAV_primate", "-l", "REF_MASTER_Dependo_AAV2", featureID]);
+				 "AL_Dependo3", "-l", "REF_MASTER_Dependo_AAV2", featureID]);
 		 });			
 		 
 	 }
