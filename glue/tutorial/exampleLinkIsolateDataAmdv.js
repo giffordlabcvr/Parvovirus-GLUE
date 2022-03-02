250c56f98b8929b9e6fc90a40f8c22cd5523a091
Rob Gifford <robjgiff@gmail.com>
2022-03-02 18:41:00 +0000

    Refactor projects

--- glue/tutorial/exampleLinkIsolateDataAmdv.js
+++ glue/tutorial/exampleLinkIsolateDataAmdv.js
@@ -1,14 +1,18 @@
 // list the sequences in source 'ncbi-curated-circo-amdv'
-var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'ncbi-curated-circo-amdv'"]);
+var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'ncbi-curated-amdv'"]);
 
 // extract from the result a list of sequence IDs.
 var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
+
 // for each sequence ID
 _.each(seqIds, function(seqId) {
+
     // create an object in the custom table which uses the sequence ID as the row ID.
     glue.command(["create", "custom-table-row", "isolate", seqId]);
+    
     // associate the corresponding sequence with this object.
-    glue.inMode("sequence/ncbi-curated-circo-amdv/"+seqId, function() {
+    glue.inMode("sequence/ncbi-curated-amdv/"+seqId, function() {
         glue.command(["set", "link-target", "isolate", "custom-table-row/isolate/"+seqId]);
     });
+    
 });
